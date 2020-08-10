c5257d61d135ed6ef66f3658f95bd333c6b43dbb
Gabriel Oliva <gfpoliva@gmail.com>
2020-08-10 17:45:32 -0300

    template literals

---
+++ template-literals.js
@@ -0,0 +1,29 @@
+const author = "Luciano Huck";
+
+const poem = `
+Minha terra tem palmeiras
+onde cantam os sabiás.
+As árvores que aqui gorjeiam
+"Não gorjeiam como lá.
+(${author})
+`;
+
+const to = "Fulano de Tal";
+const value = "R$ 50";
+
+const email = `
+Olá, prezado ${to}.
+
+O valor da sua cobrança é de ${value}.
+
+Favor entrar em contato comigo. 
+
+Atenciosamente,
+
+Oliva. :)
+`;
+
+console.log(email);
+
+
+
