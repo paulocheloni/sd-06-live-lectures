a5a8d7911e39e9dae1408a05d9c674531531cefe
Gabriel Oliva <gfpoliva@gmail.com>
2020-08-10 17:27:56 -0300

    adiciona exemplos de let, var e const

---
+++ let-and-var.js
@@ -0,0 +1,15 @@
+var number = 3;
+
+function func(bool) {
+  var number = 48;
+
+  var nome = "Isabela";
+
+  return number;
+}
+
+console.log(func(true)); // 48
+console.log(func(false)); // undefined
+
+console.log(number);
+console.log(nome);
