d43ed9c19e1961d83ab3ab380eb442eef157695e
Capi Etheriel <barraponto@gmail.com>
2021-05-13 18:51:35 -0300

    isola as respostas em fixtures

--- tests/test_responses.py
+++ tests/test_responses.py
@@ -1,31 +1,39 @@
 from unittest.mock import patch
-from depscrape.responses import make_requests
+import pytest
 from requests import Response
+from depscrape.responses import make_requests
 
-
-def test_make_requests():
-    # arrange
-    urls = ["https://pypi.org/project/black/"]
+@pytest.fixture
+def succesful_response():
     response = Response()
     response.status_code = 200
+    return response
+
+@pytest.fixture
+def failing_response():
+    response = Response()
+    response.status_code = 400
+    return response
+
+def test_make_requests(succesful_response):
+    # arrange
+    urls = ["https://pypi.org/project/black/"]
     # act
-    with patch('depscrape.responses.requests.get', return_value=response):
+    with patch('depscrape.responses.requests.get', return_value=succesful_response):
         responses = make_requests(urls)
     # assert
     assert responses[0].status_code == 200
 
 
-def test_make_requests_with_http_errors():
+def test_make_requests_with_http_errors(failing_response):
     # arrange
     urls = [
         "https://pypi.org/project/black/",
         "https://pypi.org/project/flake8/",
         "https://pypi.org/project/ipython/",
         ]
-    response = Response()
-    response.status_code = 400
     # act
-    with patch('depscrape.responses.requests.get', return_value=response):
+    with patch('depscrape.responses.requests.get', return_value=failing_response):
         responses = make_requests(urls)
     # assert
     assert len(responses) == 0
